fc5b83e1583954c3bb61d27dad159c54ff705e8c
Gustavo Morini <gustavommh@gmail.com>
2023-04-05 07:39:19 -0300

    landscape layout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cont = 1;
 
-let initial = 1;
-let final = 10;
+let initial = 501;
+let final = 520;
 
 while (initial <= final) {
   let baseEl = document.querySelector(".container-rifa").cloneNode(true);
@@ -9,12 +9,12 @@ while (initial <= final) {
   baseEl.querySelector("#numero-canhoto").textContent = initial;
   baseEl.querySelector("#numero").textContent = initial;
 
-  if (cont == 5) {
-    baseEl.style.marginBottom = "58px";
-    cont = 1;
-  } else {
-    cont++;
-  }
+  // if (cont == 5) {
+  //   baseEl.style.marginBottom = "58px";
+  //   cont = 1;
+  // } else {
+  //   cont++;
+  // }
 
   document.querySelector("#container").appendChild(baseEl);
 
