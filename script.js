ba1486aa24f0e7f96b98acefd30701cfac58fac9
Gustavo Morini <gustavommh@gmail.com>
2023-04-06 10:03:39 -0300

    ajust css media print

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cont = 1;
 
-let initial = 501;
-let final = 520;
+let initial = 541;
+let final = 560;
 
 while (initial <= final) {
   let baseEl = document.querySelector(".container-rifa").cloneNode(true);
@@ -9,12 +9,12 @@ while (initial <= final) {
   baseEl.querySelector("#numero-canhoto").textContent = initial;
   baseEl.querySelector("#numero").textContent = initial;
 
-  // if (cont == 5) {
-  //   baseEl.style.marginBottom = "58px";
-  //   cont = 1;
-  // } else {
-  //   cont++;
-  // }
+  if (cont == 10) {
+    baseEl.classList.add("breakPage");
+    cont = 1;
+  } else {
+    cont++;
+  }
 
   document.querySelector("#container").appendChild(baseEl);
 
